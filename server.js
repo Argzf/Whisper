ac4dfb8956bdb6049092bbaf3aeda6a5d405c3ff
gzf <gzf.dev@gmail.com>
2026-06-01 23:36:11 +0330

    Update server.js

--- server.js
+++ server.js
@@ -258,6 +258,18 @@ io.on('connection', (socket) => {
     await sendToDiscord(name, avatar, msg.text, clientIP, msg.file);
   });
 
+  // ==================== TYPING INDICATOR (Anonymous) ====================
+socket.on('typing', () => {
+    // Broadcast anonymously to all other users
+    socket.broadcast.emit('user_typing');
+});
+
+socket.on('stop typing', () => {
+    // Broadcast anonymously to all other users
+    socket.broadcast.emit('user_stop_typing');
+});
+// ==================== END TYPING INDICATOR ====================
+
   // Room events (disabled, but keep stubs)
   if (ROOMS_ENABLED) {
     // full room logic would go here
